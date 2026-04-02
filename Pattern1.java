df0e7a6c00cefb07dd70167805c15499625b9b0a
AkiiAngalakuduru <akhilangalakuduru005@gmail.com>
2026-04-02 15:33:16 +0530

    Added Pattern1 Java program

---
+++ Pattern1.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String args[]){
+       pattern1(5); 
+    }
+    static void pattern1(int n){
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=row;col++){
+                System.out.print("* ");
+            }System.out.println();
+        }
+    }
+}
